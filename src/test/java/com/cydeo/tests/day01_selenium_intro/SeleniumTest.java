f0bd2587dedca724a5e636cf4928ab4801116821
bekir <bkrylmz981@gmail.com>
2022-12-19 11:37:23 +0300

    first added

---
+++ src/test/java/com/cydeo/tests/day01_selenium_intro/SeleniumTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.day01_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://facebook.com");
+
+
+
+    }
+
+}
